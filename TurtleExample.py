d8d677e651f4d4739206140f93c24f5c4e4eef8e
Seth J. Connell <sethjordanconnell@gmail.com>
2017-11-28 18:41:42 -0600

    Added Python File

---
+++ TurtleExample.py
@@ -0,0 +1,14 @@
+import turtle
+
+def draw_something():
+    window = turtle.Screen()
+    window.bgcolor("black")
+
+    seth = turtle.Turtle()
+    seth.shape("turtle")
+    seth.color("blue")
+    
+    seth.forward(50)
+
+
+draw_something()
